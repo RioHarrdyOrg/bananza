57013a26d4e1d360052bbd44aeecc0fa4cc002c3
RioHarrdy <vladislavdudar1995@gmail.com>
2021-06-24 00:09:17 +0300

    add changes text

--- app/js/common.js
+++ app/js/common.js
@@ -547,16 +547,6 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 	}
 
-	function throttle(fn, wait) {
-		var time = Date.now();
-		return function () {
-			if (time + wait - Date.now() < 0) {
-				fn();
-				time = Date.now();
-			}
-		};
-	}
-
 	if ($(".slick").length) {
 		$(".slick").slick({
 			dots: false,
